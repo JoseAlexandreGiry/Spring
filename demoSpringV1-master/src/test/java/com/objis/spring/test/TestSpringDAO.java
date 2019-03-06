ac569ce95237ae11fe25506e616da5aa4dea27ab
JoseAlexandreGiry <33418836+JoseAlexandreGiry@users.noreply.github.com>
2019-03-06 15:51:51 +0100

    Add files via upload

---
+++ demoSpringV1-master/src/test/java/com/objis/spring/test/TestSpringDAO.java
@@ -0,0 +1,50 @@
+package com.objis.spring.test;
+
+import com.objis.spring.demodao.IFormationDao;
+import com.objis.spring.demodomaine.Formation;
+import junit.framework.TestCase;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+public class TestSpringDAO extends TestCase {
+
+    private Formation form;
+    private IFormationDao springDao;
+    private ClassPathXmlApplicationContext appContext;
+
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp ();
+        form = new Formation ( 1, "GTM" );
+        appContext = new ClassPathXmlApplicationContext("demo-beans.xml");
+        springDao = (IFormationDao) appContext.getBean("IFormationDao");
+
+    }
+
+ /*   @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        form = null;
+        appContext = null;
+        springDao = null;
+    }*/
+
+    public void testSaveFormation() {
+        Formation result = springDao.save(form);
+        assertTrue ( true );
+        System.out.println ("Formation ajoutée en base : " + result.getTheme());
+
+    }
+
+    public void testfindTheme() {
+        springDao = (IFormationDao) appContext.getBean("IFormationDao");
+        List<Formation> formation = springDao.findByTheme(form.getTheme());
+        for (Formation e : formation) {
+            System.out.println ("Thème present en base : " + e.getTheme());
+        }
+        assertNotNull(formation);
+    }
+}
+
